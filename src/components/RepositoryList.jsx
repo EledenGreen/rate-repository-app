a3c55dac775be0d88fd03ed86b0682830deee6df
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-11-05 21:12:54 +0530

    Rest fetch working

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,6 @@
 import { FlatList, View, StyleSheet } from 'react-native'
 import RepositoryItem from './RepositoryItem'
+import useRepositories from '../hooks/useRepositories'
 
 const styles = StyleSheet.create({
   separator: {
@@ -60,10 +61,16 @@ const repositories = [
 const ItemSeparator = () => <View style={styles.separator} />
 
 const RepositoryList = () => {
+  const { repositories } = useRepositories()
+
+  const repositoryNodes = repositories
+    ? repositories.edges.map((edge) => edge.node)
+    : []
+
   return (
     <View style={styles.container}>
       <FlatList
-        data={repositories}
+        data={repositoryNodes}
         ItemSeparatorComponent={ItemSeparator}
         renderItem={({ item }) => <RepositoryItem item={item} />}
       />
