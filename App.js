c60693c54f9085b862df8c10f9964718d8c000e8
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-11-06 12:37:03 +0530

    graphql setup

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { NativeRouter } from 'react-router-native'
-import Main from './src/components/Main'
 import { StatusBar } from 'expo-status-bar'
+import { ApolloProvider } from '@apollo/client'
+
+import Main from './src/components/Main'
+import createApolloClient from './src/utils/apolloClient'
+
+const apolloClient = createApolloClient()
+
 const App = () => {
   return (
     <>
       <StatusBar style="auto" />
 
       <NativeRouter>
-        <Main />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
     </>
   )
